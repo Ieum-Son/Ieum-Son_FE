f29bd7f0ef2cebad8eb92f481723dd7c9eceaade
taeyeon <binnimhi@naver.com>
2026-07-05 00:38:14 +0900

    feat:: splash 화면 퍼블리싱

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -1,20 +1,38 @@
 import Blue from "@/assets/Logo/Blue.png";
-import React from "react";
+import { LoginButton, SignupButton } from "@/components/splash/index";
 import { Image } from "react-native";
 import styled from "styled-components/native";
 
 export default function Splash() {
   return (
-    <Container>
-      <Image source={Blue} resizeMode="contain" />
-    </Container>
+    <Wrapper>
+      <Container>
+        <Image source={Blue} resizeMode="contain" />
+
+        <ButtonWrapper>
+          <SignupButton />
+          <LoginButton />
+        </ButtonWrapper>
+      </Container>
+    </Wrapper>
   );
 }
 
+const Wrapper = styled.View`
+  height: 100%;
+  justify-content: center;
+`;
+
 const Container = styled.View`
   width: 100%;
-  height: 100%;
+  height: 80%;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+`;
+
+const ButtonWrapper = styled.View`
+  display: flex;
+  gap: 15px;
+  margin-bottom: -20px;
 `;
