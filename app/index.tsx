5319d6bfe77d43ac2c5a9ab947d64538d104e242
taeyeon <binnimhi@naver.com>
2026-07-05 00:40:23 +0900

    feat:: 앱 시작시 초기화면 퍼블리싱 및 navigation 추가

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import Blue from "@/assets/Logo/Blue.png";
+import { router } from "expo-router";
+import React, { useEffect } from "react";
+import { Image } from "react-native";
+import styled from "styled-components/native";
+
+export default function Splash() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/Splash");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Container>
+      <Image source={Blue} resizeMode="contain" />
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
