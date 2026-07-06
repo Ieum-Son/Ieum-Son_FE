50beb28fde3d963b274cac825acb1be15c1f57fd
taeyeon <binnimhi@naver.com>
2026-07-06 19:46:32 +0900

    fix:: AuthButton 불러오기 경로 수정

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,4 +1,4 @@
-import AuthButton from "@/components/auth/AuthButton";
+import AuthButton from "@/components/auth/Button";
 import Input from "@/components/auth/LoginInput";
 import Question from "@/components/auth/Question";
 import { colors } from "@/constants/colors";
