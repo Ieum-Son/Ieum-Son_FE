b1b089377f69899896048282d47036a123b85524
taeyeon <binnimhi@naver.com>
2026-07-18 00:53:05 +0900

    feat:: QueryClientProvider SafeArea 설정 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,23 @@
 import { queryClient } from "@/libs/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-export default function Rootlayout() {
+export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaView style={{ flex: 1 }} edges={["top", "bottom"]}>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              contentStyle: {
+                backgroundColor: "#ffffff",
+              },
+            }}
+          />
+        </SafeAreaView>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
