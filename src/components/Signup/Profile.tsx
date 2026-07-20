aa1d17d75270b29a3aa475ce1869e8ed50a6c1a5
taeyeon <binnimhi@naver.com>
2026-07-20 19:58:14 +0900

    feat:: profile 설정 페이지에 이미지 업로드 기능 추가

--- src/components/Signup/Profile.tsx
+++ src/components/Signup/Profile.tsx
@@ -1,20 +1,52 @@
 import { Image } from "expo-image";
-import React from "react";
-import { Pressable } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 import styled from "styled-components/native";
 
-export default function Profile({}) {
-  //이미지 추가 로직 구현
+export default function Profile() {
+  const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
+  const [imageUrl, setImageUrl] = useState("");
+
+  const uploadImage = async () => {
+    //권한 확인
+    if (!status?.granted) {
+      const permission = await requestPermission();
+
+      if (!permission.granted) {
+        return;
+      }
+    }
+
+    //이미지 업로드
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: false,
+      quality: 1,
+    });
+
+    if (result.canceled) {
+      return;
+    }
+
+    //이미지 업로드 한 거 표시
+    setImageUrl(result.assets[0].uri);
+  };
+
   return (
-    <>
-      <Wrapper>
-        <UserProfile
-          source={require("@/assets/user/defaultProfile.png")}
-        ></UserProfile>
-        <Overlay />
-        <Camera source={require("@/assets/user/camera.png")}></Camera>
-      </Wrapper>
-    </>
+    <Wrapper onPress={uploadImage}>
+      <UserProfile
+        source={
+          imageUrl
+            ? { uri: imageUrl }
+            : require("@/assets/user/defaultProfile.png")
+        }
+      />
+      {!imageUrl && (
+        <Overlay pointerEvents="none">
+          <Camera source={require("@/assets/user/camera.png")} />
+        </Overlay>
+      )}
+    </Wrapper>
   );
 }
 
@@ -24,9 +56,11 @@ const Overlay = styled.View`
   height: 120px;
   border-radius: 60px;
   background-color: rgba(0, 0, 0, 0.4);
+  align-items: center;
+  justify-content: center;
 `;
 
-const Wrapper = styled(Pressable)`
+const Wrapper = styled.Pressable`
   width: 120px;
   height: 120px;
   position: relative;
@@ -41,9 +75,4 @@ const UserProfile = styled(Image)`
 const Camera = styled(Image)`
   width: 48px;
   height: 48px;
-
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-24px, -24px);
 `;
