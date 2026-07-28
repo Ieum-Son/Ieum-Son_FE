50f26f8e63351f7883a9b600086545fa93b429c6
taeyeon <binnimhi@naver.com>
2026-07-28 21:34:10 +0900

    feat:: 시작화면 설정

--- app/index.tsx
+++ app/index.tsx
@@ -7,8 +7,8 @@ import styled from "styled-components/native";
 export default function Splash() {
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.replace("/Profile/FavoritesWords");
-    }, 0);
+      router.replace("/Splash");
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
