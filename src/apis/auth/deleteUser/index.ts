f06438166bb1adc0324e0ee14dc6576718d3258c
taeyeon <binnimhi@naver.com>
2026-08-18 23:01:11 +0900

    chore:: login -> delete

--- src/apis/auth/deleteUser/index.ts
+++ src/apis/auth/deleteUser/index.ts
@@ -1,9 +1,10 @@
 import { api } from "@/apis";
-import type { LoginRequestProps } from "./type";
+import type { DeleteUserRequest } from "./type";
 
-export const login = async ({ password }: LoginRequestProps) => {
-  const response = await api.post(`/api/auth/account`, {
-    password,
+export const deleteUser = async ({ password }: DeleteUserRequest) => {
+  const response = await api.delete<void>("/api/auth/account", {
+    data: { password },
   });
+
   return response.data;
 };
