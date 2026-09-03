938c81c1623f318b22af69806f84dc3dc5514627
taeyeon <binnimhi@naver.com>
2026-09-03 20:18:49 +0900

    feat:: form-data 헤더 포함, null 값 제거 및 이미지 없을 경우 예외처리 추가

--- src/apis/changeProfile/index.ts
+++ src/apis/changeProfile/index.ts
@@ -2,17 +2,20 @@ import { api } from "@/apis";
 import { ChangeProfileProps, ChangeProfileResponse } from "./type";
 
 const ChangeProfile = async ({ img }: ChangeProfileProps) => {
+  if (!img?.uri) throw new Error("업로드할 이미지가 없습니다.");
+
   const formData = new FormData();
 
   formData.append("image", {
-    uri: img?.uri || null,
-    name: img?.name || null,
-    type: img?.type || null,
-  } as any);
+    uri: img.uri,
+    name: img.name,
+    type: img.type,
+  } as unknown as Blob);
 
   const response = await api.patch<ChangeProfileResponse>(
     "/api/members/me/profile",
     formData,
+    { headers: { "Content-Type": "multipart/form-data" } },
   );
 
   return response.data;
