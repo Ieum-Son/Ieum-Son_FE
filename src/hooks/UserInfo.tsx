6a6ddd203b7cd38019260ccbdf66557ef4a075f0
taeyeon <binnimhi@naver.com>
2026-09-08 15:03:25 +0900

    feat:: 사용자 정보 조회 커스텀훅 추가

---
+++ src/hooks/UserInfo.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import UserInfo from "@/apis/userInfo";
+import { UserInfoResponse } from "@/apis/userInfo/type";
+import { useQuery } from "@tanstack/react-query";
+import { isAxiosError } from "axios";
+import { ErrorResponse } from "./errorResponse";
+import { useUserStore } from "@/stores/userStore";
+
+export const getUserInfo = (error: unknown) => {
+  if (!isAxiosError<ErrorResponse>(error)) {
+    return error instanceof Error
+      ? error.message
+      : "알 수 없는 오류가 발생했습니다.";
+  }
+  const status = error.response?.status;
+
+  if (status === 401) return "인증이 필요합니다.";
+  if (status === 404) return "해당 유저가 존재하지 않습니다.";
+  return "사용자 정보를 불러오는 데 실패했습니다.";
+};
+
+export const useUserInfo = () => {
+  const setUser = useUserStore((state) => state.setUser);
+  const user = useQuery<UserInfoResponse, Error>({
+    queryKey: ["user"],
+    queryFn: UserInfo,
+  });
+
+  useEffect(() => {
+    if (!user.data) return;
+
+    setUser({
+      email: user.data.email,
+      name: user.data.name,
+      loginId: user.data.loginId,
+      profileImageUrl: user.data.profileImageUrl ?? null,
+    });
+  }, [user.data, setUser]);
+
+  return user;
+};
