d05914bc1420f0bed2fbe2df7196a7256febf2de
taeyeon <binnimhi@naver.com>
2026-09-19 21:33:00 +0900

    fix:: 비밀번호 토글 아이콘이 입력창 밖으로 밀리는 문제 수정

--- src/components/auth/LoginInput.tsx
+++ src/components/auth/LoginInput.tsx
@@ -43,6 +43,7 @@ export default function LoginInput({
 const Container = styled.View`
   background-color: ${colors.neutral100};
   width: 353px;
+  max-width: 100%;
   height: 48px;
   border-radius: 12px;
   border: 1px solid ${colors.neutral100};
